cdee26412b8b8e5adfca8a424cde2911da6c09da
hermetic-echoes <273742024+hermetic-echoes@users.noreply.github.com>
2026-04-07 22:08:09 +0100

    Music.NoteModifier class added

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
 (() => {
+	class Music {
+		static NoteModifier = class NoteModifier {
+			static __b = Symbol();
+			static __n = Symbol();
+			static __s = Symbol();
+
+			static as_display_string(variant) {
+				switch (variant) {
+					case Music.NoteModifier.__b:
+						return '♭';
+					case Music.NoteModifier.__n:
+						return '♮';
+					case Music.NoteModifier.__s:
+						return '♯';
+				}
+			}
+
+			static as_keyboard_friendly_string(variant) {
+				switch (variant) {
+					case Music.NoteModifier.__b:
+						return 'b';
+					case Music.NoteModifier.__n:
+						return '';
+					case Music.NoteModifier.__s:
+						return 's';
+				}
+			}
+		};
+	}
 })();
