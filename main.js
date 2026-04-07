9edc2214458768b72481760ff2ba1b71969ae1fd
hermetic-echoes <273742024+hermetic-echoes@users.noreply.github.com>
2026-04-07 22:11:12 +0100

    Music.NoteLetter class added

--- main.js
+++ main.js
@@ -27,5 +27,53 @@
 				}
 			}
 		};
+
+		static NoteLetter = class NoteLetter {
+			static __c = Symbol();
+			static __d = Symbol();
+			static __e = Symbol();
+			static __f = Symbol();
+			static __g = Symbol();
+			static __a = Symbol();
+			static __b = Symbol();
+
+			static as_display_string(variant) {
+				switch (variant) {
+					case Music.NoteLetter.__c:
+						return 'C';
+					case Music.NoteLetter.__d:
+						return 'D';
+					case Music.NoteLetter.__e:
+						return 'E';
+					case Music.NoteLetter.__f:
+						return 'F';
+					case Music.NoteLetter.__g:
+						return 'G';
+					case Music.NoteLetter.__a:
+						return 'A';
+					case Music.NoteLetter.__b:
+						return 'B';
+				}
+			}
+
+			static as_keyboard_friendly_string(variant) {
+				switch (variant) {
+					case Music.NoteLetter.__c:
+						return 'c';
+					case Music.NoteLetter.__d:
+						return 'd';
+					case Music.NoteLetter.__e:
+						return 'e';
+					case Music.NoteLetter.__f:
+						return 'f';
+					case Music.NoteLetter.__g:
+						return 'g';
+					case Music.NoteLetter.__a:
+						return 'a';
+					case Music.NoteLetter.__b:
+						return 'b';
+				}
+			}
+		};
 	}
 })();
