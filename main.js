b43a9ba604eecb835c7d16c9e29ba7d9330b3da9
hermetic-echoes <273742024+hermetic-echoes@users.noreply.github.com>
2026-04-07 22:30:36 +0100

    Music.Note class added

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 (() => {
+	class StringType {
+		static __display = Symbol();
+		static __keyboard_friendly = Symbol();
+	}
+
 	class Music {
 		static NoteModifier = class NoteModifier {
 			static __b = Symbol();
 			static __n = Symbol();
 			static __s = Symbol();
 
-			static as_display_string(variant) {
+			static as_string(variant, type = StringType.__display) {
+				switch (type) {
+					case StringType.__display:
+						return Music.NoteModifier.#as_display_string(variant);
+					case StringType.__keyboard_friendly:
+						return Music.NoteModifier.#as_keyboard_friendly_string(variant);
+				}
+			}
+
+			static #as_display_string(variant) {
 				switch (variant) {
 					case Music.NoteModifier.__b:
 						return '♭';
@@ -16,7 +30,7 @@
 				}
 			}
 
-			static as_keyboard_friendly_string(variant) {
+			static #as_keyboard_friendly_string(variant) {
 				switch (variant) {
 					case Music.NoteModifier.__b:
 						return 'b';
@@ -24,6 +38,27 @@
 						return '';
 					case Music.NoteModifier.__s:
 						return 's';
+				}
+			}
+
+			static from_int(value) {
+				if (value < 0) {
+					return Music.NoteModifier.__b;
+				} else if (value == 0) {
+					return Music.NoteModifier.__n;
+				} else {
+					return Music.NoteModifier.__s;
+				}
+			}
+
+			static to_int(variant) {
+				switch (variant) {
+					case Music.NoteModifier.__b:
+						return -1;
+					case Music.NoteModifier.__n:
+						return 0;
+					case Music.NoteModifier.__s:
+						return 1;
 				}
 			}
 		};
@@ -37,7 +72,16 @@
 			static __a = Symbol();
 			static __b = Symbol();
 
-			static as_display_string(variant) {
+			static as_string(variant, type = StringType.__display) {
+				switch (type) {
+					case StringType.__display:
+						return Music.NoteLetter.#as_display_string(variant);
+					case StringType.__keyboard_friendly:
+						return Music.NoteLetter.#as_keyboard_friendly_string(variant);
+				}
+			}
+
+			static #as_display_string(variant) {
 				switch (variant) {
 					case Music.NoteLetter.__c:
 						return 'C';
@@ -56,7 +100,7 @@
 				}
 			}
 
-			static as_keyboard_friendly_string(variant) {
+			static #as_keyboard_friendly_string(variant) {
 				switch (variant) {
 					case Music.NoteLetter.__c:
 						return 'c';
@@ -73,6 +117,53 @@
 					case Music.NoteLetter.__b:
 						return 'b';
 				}
+			}
+		};
+
+		static Note = class Note {
+			#letter;
+			#modifier;
+
+			constructor(letter, modifier) {
+				this.#letter = letter;
+				this.#modifier = modifier;
+			}
+
+			as_string(type) {
+				return `${this.#letter__as_string(type)}${this.#modifier__as_string(type)}`;
+			}
+
+			#modifier__as_string(type) {
+				let as_string = '';
+				
+				const modifier = Music.NoteModifier.from_int(this.#modifier);
+
+				switch (modifier) {
+					case Music.NoteModifier.__b:
+						for (let i = this.#modifier; i < 0; i++) {
+							as_string += Music.NoteModifier.as_string(modifier, type);
+						}
+						break;
+					case Music.NoteModifier.__n:
+						as_string = Music.NoteModifier.as_string(modifier, type);
+						break;
+					case Music.NoteModifier.__s:
+						let as_string = '';
+						for (let i = 1; i <= this.modifier; i++) {
+							as_string += Music.NoteModifier.as_string(modifier, type);
+						}
+						break;
+				}
+
+				return as_string;
+			}
+
+			#letter__as_string(type) {
+				return Music.NoteLetter.as_string(this.#letter, type);
+			}
+
+			new__diatonic_root(letter, modifier) {
+				return new Music.Note(letter, Music.NoteModifier.to_int(modifier));
 			}
 		};
 	}
